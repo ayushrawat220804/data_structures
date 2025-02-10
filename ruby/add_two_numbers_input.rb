5a992ca5b849ca8e49fc46be129bfbee5263e5f8
Ayush Rawat <ayushrawat220804@email.com>
2025-02-10 14:44:54 +0530

    add numbers using input

---
+++ ruby/add_two_numbers_input.rb
@@ -0,0 +1,11 @@
+puts "programe to add two numbers using input"
+
+print " enter first number : " 
+nums1 = gets.chomp.to_i
+
+print "enter second number : "
+nums2 = gets.chomp.to_i
+
+result = nums1 + nums2
+
+puts "addition of nums1 #{nums1} and #{nums2} is #{result}"
