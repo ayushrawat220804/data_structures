1481ab0419b0706f2529f5ca89ff849867ec4eef
Ayush Rawat <ayushrawat220804@email.com>
2025-02-10 12:19:32 +0530

    ruby programming

---
+++ ruby/add_two_numbers.rb
@@ -0,0 +1,8 @@
+puts "a simple programme to print two numbers"
+
+nums1 = 10
+nums2 = 20
+
+result = nums1 + nums2
+
+puts "you entered #{nums1} and #{nums2} their result is : #{result}"
---
+++ ruby/hello_world.rb
@@ -0,0 +1,13 @@
+print "Hello"
+print " world"
+# `print` outputs text without adding a new line.
+# So the output will be: Hello world
+
+puts  # This adds a blank line
+
+puts "Hello world"
+# `puts` automatically adds a newline after printing.
+# The output will be:
+# Hello world
+#
+# (A newline is added after "Hello world")
