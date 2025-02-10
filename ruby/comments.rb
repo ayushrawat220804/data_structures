04d68654f3c94971e3a892312e00ae2bf7b15906
Ayush Rawat <ayushrawat220804@email.com>
2025-02-10 14:43:59 +0530

    comments in ruby

---
+++ ruby/comments.rb
@@ -0,0 +1,12 @@
+puts "Types of comments in Ruby"
+
+puts "Single-line comments using #"
+# This is a single-line comment
+
+puts "Multi-line comments using =begin and =end"
+=begin
+This is a 
+multi-line comment.
+Everything between =begin and =end is ignored by Ruby.
+=end
+puts "Hello world !!"
